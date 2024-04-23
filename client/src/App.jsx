e0be027a5e0a281d4f7f2f4e4c718c83e7026622
ferronmol <proyectosferron@gmail.com>
2024-04-23 15:12:31 +0200

    start client stucture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1>My React App!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 className="text-center mt-5">Welcome to Asopac </h1>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
