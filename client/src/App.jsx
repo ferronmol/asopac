8becdfad31e6768f450e0d71b67d63ded1139a7f
ferronmol <proyectosferron@gmail.com>
2024-04-24 15:13:22 +0200

    start client stucture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,39 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={<h1 className="text-center mt-5">Welcome to Asopac </h1>}
+          element={<h1 className="text-center mt-5">Bienvenido a Asopac </h1>}
+        />
+        <Route
+          path="/association/register"
+          element={
+            <h1 className="text-center mt-5">
+              Página de registro de Asociaciones de Pacientes
+            </h1>
+          }
+        />
+        <Route
+          path="/association/login"
+          element={
+            <h1 className="text-center mt-5">
+              Página de acceso a su Asociacion de Pacientes
+            </h1>
+          }
+        />
+        <Route
+          path="/association/profile"
+          element={<h1 className="text-center mt-5">Profile</h1>}
+        />
+        <Route
+          path="/association/logout"
+          element={<h1 className="text-center mt-5">Logout</h1>}
+        />
+        <Route
+          path="/users/register"
+          element={<h1 className="text-center mt-5">User Register</h1>}
+        />
+        <Route
+          path="/users/login"
+          element={<h1 className="text-center mt-5">User Login</h1>}
         />
       </Routes>
     </BrowserRouter>
